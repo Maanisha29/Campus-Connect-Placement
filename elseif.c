fdc620737a34d3cae77080ef51bfd063e935bca8
Maanisha G <maanisha.govindasamy@gmail.com>
2026-07-17 20:30:47 +0530

    Add files via upload

--- elseif.c
+++ elseif.c
@@ -1,1 +1,16 @@
+//Creates elseif.c file
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a==0)
+    printf("0")
+else if(a==1)
+    printf("1")
+else if(a==2)
+    printf("2")
+else
+    print("Wrong Input")
 
+}
